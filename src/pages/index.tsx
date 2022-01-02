3543c52f0dfdeb1ef4fed96c9baa88fc50fed284
Paulo Chaves <opchav@gmail.com>
2022-01-02 18:50:20 -0300

    fix period selector on dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,72 @@
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { blue, green, red } from "@mui/material/colors";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { blue, green, red } from '@mui/material/colors';
 
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
-import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
-import { MyAppState } from "../types/my-app";
-import { getLayout } from "../components/layouts/dashboard/Dashboard";
-import MonthSelector from "../components/MonthSelector";
-import { useTransactions } from "../context/TransactionContext";
+import { MyAppState } from '@/types/my-app';
+import { getLayout } from '@/components/layouts/dashboard/Dashboard';
+import MonthSelector from '@/components/MonthSelector';
+import { useTransactions } from '@/context/TransactionContext';
 
-// TODO `currentMonth` and `setCurrentMonth` are not used directly here neither on the transactions page component
-// those two can potentially be promoted to a context
-
-export default function DashboardPage({
-  currentMonth,
-  setCurrentMonth,
-}: MyAppState) {
+export default function DashboardPage({ currentPeriod, setCurrentPeriod }: MyAppState) {
   return (
     <>
       <Grid container spacing={3} sx={{ pb: 3 }}>
         <Grid item xs={12}>
-          <Box sx={{ display: "flex", alignItems: "center", ml: -2 }}>
-            <MonthSelector
-              currentMonth={currentMonth}
-              setCurrentMonth={setCurrentMonth}
-            />
+          <Box sx={{ display: 'flex', alignItems: 'center', ml: -4 }}>
+            <MonthSelector currentPeriod={currentPeriod} setCurrentPeriod={setCurrentPeriod} />
           </Box>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid item container xs={12} md={8} spacing={3}>
           <Grid item xs={12} sm={4} md={4}>
-            <Paper sx={{ p: 2, display: "flex" }} elevation={0}>
+            <Paper sx={{ p: 2, display: 'flex' }} elevation={0}>
               <Box sx={{ pr: 1 }}>
                 <IconButton>
                   <ArrowCircleUpIcon sx={{ color: green[800] }} />
                 </IconButton>
               </Box>
               <Box>
-                <Typography sx={{ fontSize: "0.85rem" }}>Incomes</Typography>
+                <Typography sx={{ fontSize: '0.85rem' }}>Incomes</Typography>
                 <Typography sx={{ fontWeight: 500 }}>R$ 3850.90</Typography>
               </Box>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4} md={4}>
-            <Paper sx={{ p: 2, display: "flex" }} elevation={0}>
+            <Paper sx={{ p: 2, display: 'flex' }} elevation={0}>
               <Box sx={{ pr: 1 }}>
                 <IconButton>
                   <ArrowCircleDownIcon sx={{ color: red[800] }} />
                 </IconButton>
               </Box>
               <Box>
-                <Typography sx={{ fontSize: "0.85rem" }}>Expenses</Typography>
+                <Typography sx={{ fontSize: '0.85rem' }}>Expenses</Typography>
                 <Typography sx={{ fontWeight: 500 }}>R$ 3850.90</Typography>
               </Box>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4} md={4}>
-            <Paper sx={{ p: 2, display: "flex" }} elevation={0}>
+            <Paper sx={{ p: 2, display: 'flex' }} elevation={0}>
               <Box sx={{ pr: 1 }}>
                 <IconButton>
                   <AccountBalanceIcon sx={{ color: blue[700] }} />
                 </IconButton>
               </Box>
               <Box>
-                <Typography sx={{ fontSize: "0.85rem" }}>Balances</Typography>
+                <Typography sx={{ fontSize: '0.85rem' }}>Balances</Typography>
                 <Typography sx={{ fontWeight: 500 }}>R$ 3850.90</Typography>
               </Box>
             </Paper>
@@ -86,8 +77,8 @@ export default function DashboardPage({
           <Paper
             sx={{
               p: 2,
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column',
               height: 240,
             }}
             elevation={0}
@@ -100,8 +91,8 @@ export default function DashboardPage({
           <Paper
             sx={{
               p: 2,
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column',
               height: 240,
             }}
             elevation={0}
@@ -113,8 +104,8 @@ export default function DashboardPage({
           <Paper
             sx={{
               p: 2,
-              display: "flex",
-              flexDirection: "column",
+              display: 'flex',
+              flexDirection: 'column',
               height: 240,
             }}
             elevation={0}
@@ -124,10 +115,7 @@ export default function DashboardPage({
         </Grid>
         {/* Recent Orders */}
         <Grid item xs={12}>
-          <Paper
-            sx={{ p: 2, display: "flex", flexDirection: "column" }}
-            elevation={0}
-          >
+          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }} elevation={0}>
             <LatestTransactionsTable />
           </Paper>
         </Grid>
@@ -155,10 +143,7 @@ function LatestTransactionsTable() {
         </TableHead>
         <TableBody>
           {transactions.slice(0, 5).map((row) => (
-            <TableRow
-              key={row.title}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
+            <TableRow key={row.title} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row">
                 {row.title}
               </TableCell>
