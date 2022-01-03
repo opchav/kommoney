652c02abf96bd54d9b59416daa08f5bd7453d1d8
Paulo Chaves <opchav@gmail.com>
2022-01-03 15:29:42 -0300

    transaction form with react-hook-form

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,3 +1,126 @@
+import * as React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import ArticleIcon from '@mui/icons-material/Article';
+import { useTransactionsDispatch } from '../context/TransactionContext';
+import {
+  FormControl,
+  FormControlLabel,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Stack,
+  Switch,
+} from '@mui/material';
+
+const ModalBox = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  top: '40%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(2),
+}));
+
+type TransactionInput = {
+  value: number;
+  title: string;
+  paid: boolean;
+};
+
+const initTransaction: TransactionInput = {
+  title: '',
+  value: 10,
+  paid: false,
+};
+
+export default function TransactionForm() {
+  const [open, setOpen] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<TransactionInput>();
+
+  const onSubmit: SubmitHandler<TransactionInput> = (data) => {
+    console.log('>>>', data);
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+        Add
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ModalBox>
+          <Typography component="h1" variant="h5">
+            New expense
+          </Typography>
+          <Stack component="form" onSubmit={handleSubmit(onSubmit)}>
+            <Stack direction="row" alignItems="flex-end">
+              <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                <InputLabel htmlFor="standard-adornment-amount">Value</InputLabel>
+                <Input
+                  id="tx-value"
+                  type="number"
+                  defaultValue={initTransaction.value}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <AttachMoneyIcon />
+                    </InputAdornment>
+                  }
+                  {...register('value')}
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Switch name="paid" {...register('paid')} id="tx-paid" sx={{ ml: 1 }} />}
+                label="Paid"
+              />
+            </Stack>
+
+            <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-amount">Description</InputLabel>
+              <Input
+                id="tx-title"
+                defaultValue={initTransaction.title}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <ArticleIcon />
+                  </InputAdornment>
+                }
+                {...register('title')}
+              />
+            </FormControl>
+
+            <Box sx={{ display: 'flex', pt: 2, justifyContent: 'flex-end' }}>
+              <Button variant="contained" type="submit">
+                Save
+              </Button>
+            </Box>
+          </Stack>
+        </ModalBox>
+      </Modal>
+    </>
+  );
+}
+
+/*
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
@@ -143,3 +266,4 @@ export default function TransactionForm() {
     </>
   );
 }
+*/
--- src/types/transaction.d.ts
+++ src/types/transaction.d.ts
@@ -2,10 +2,16 @@ export interface Transaction {
   id: string;
   title: string;
   value: number;
-  type: "income" | "expense";
+  type: TransactionType;
   date: number;
   paid: number;
   category: string;
   account: string;
   note: string;
+}
+
+export enum TransactionType {
+  EXPENSE = 'expense',
+  INCOME = 'income',
+  TRANSFER = 'transfer',
 }
