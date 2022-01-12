cc5a96888f79cdf1815edaf22572ad550d15b040
opchav <opchav@gmail.com>
2022-01-12 18:37:21 -0300

    add edit transaction endpoint

--- src/pages/api/transactions/[id].ts
+++ src/pages/api/transactions/[id].ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/utils/prisma';
 import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
-import { TransactionType } from '@prisma/client';
+import { Prisma, Transaction, TransactionType } from '@prisma/client';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const id = req.query.id as string;
@@ -52,6 +52,84 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       return res.status(204).send('');
     }
 
+    // NOTE: txAccount can't be changed.
+    //       -- TODO: support txAccount change (change balance of the current and new account is needed)
+    if (req.method === 'PUT' || req.method === 'PATCH') {
+      const bodyTx = req.body as Transaction;
+      const dbTx = await getTransaction(id, session);
+
+      const transaction = await prisma.$transaction(async (prisma) => {
+
+        const account = await prisma.txAccount.findUnique({ where: { id: dbTx.txAccountId } })
+        let balance = account.balance;
+        const value = new Prisma.Decimal(bodyTx.value);
+        const txValue = dbTx.value;
+
+        // It wasn't paid and now it is still not
+        if (!dbTx.paid && !bodyTx.paid) {
+          // no changes to account balance
+        }
+
+        // It wasn't paid and now it is
+        //   - if expense should decrease the balance
+        //     balance = 1000, value = 100, new balance = 900
+        //   - if income should increase the balance
+        //     balance = 1000, value = 100, new balance = 1100
+        if (!dbTx.paid && bodyTx.paid) {
+          if (dbTx.type === TransactionType.EXPENSE) {
+            balance = balance.sub(value)
+          } else {
+            balance = balance.add(value);
+          }
+        }
+
+        // It was paid and now it is not
+        //   - if expense should increase the balance
+        //     balance = 1000, value = 100, new balance = 1100
+        //   - if income should decrease the balance
+        //     balance = 1000, value = 100, new balance = 900
+        if (dbTx.paid && !bodyTx.paid) {
+          if (dbTx.type === TransactionType.EXPENSE) {
+            balance = balance.add(txValue)
+          } else {
+            balance = balance.sub(txValue);
+          }
+        }
+
+        // it was paid and now it is still paid
+        // only change the balance if the value is different
+        if (dbTx.paid && bodyTx.paid && value !== txValue) {
+          if (dbTx.type === TransactionType.EXPENSE) {
+            // add existing tx value to return balance to previous state then decrease the new value
+            balance = balance.add(txValue).sub(value);
+          } else {
+            balance = balance.sub(txValue).add(value);
+          }
+        }
+
+        if (balance !== account.balance) {
+          await prisma.txAccount.update({
+            where: { id: account.id },
+            data: { balance }
+          })
+        }
+
+        return prisma.transaction.update({
+          where: { id: dbTx.id },
+          data: {
+            value,
+            description: bodyTx.description,
+            paid: bodyTx.paid,
+            note: bodyTx.note,
+            categoryId: bodyTx.categoryId,
+          }
+        })
+
+      })
+
+      return res.status(200).json({ transaction })
+    }
+
     return res.status(405).json({ message: '405 - Method Not Allowed' });
   } catch (error) {
     console.log(error);
@@ -70,3 +148,4 @@ async function getTransaction(id: string, session: Session) {
     },
   });
 }
+
